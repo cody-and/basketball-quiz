9d88f63e6b0d40cf370cc4ecc22e57fffd0453a1
cody-and <codya.0807@icloud.com>
2023-07-05 23:57:34 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+/*
+
+What data type can I use to store all the questions, and the answers that go with them?
+How can I present one set of question + answers on the screen one at a time, and move from to another?
+
+How can I tell whether the user has clicked on the right answer?
+How can I save high scores?
+
+Test with just 1-2 questions
+
+
+Set it up so that the display of the question and answers is the same, no matter which one is being displayed
+
+Things to practice:
+----------------------
+1. Create DOM elements and display them on the screen 
+2. Write a function that accepts an object, and displays the parts of the object on the screen
+3. Write a function that detects when a button is clicked, and provides details about that button
+
+
+*/
+
